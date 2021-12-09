4266627b4ac5776df7c0986a778371f47396b893
PabloFM <pafmon@gmail.com>
2021-12-09 18:08:02 +0100

    first codebase

---
+++ controllers/apiv1recipesControllerService.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports.getRecipes = function getRecipes(req, res, next) {
+  var data = [
+    {
+      "recipeId": "espetos",
+      "description": "Sardinas a la brasa ensartadas en un palo",
+      "ingredients": "Sardina, sal y brasas",
+      "complexity": "media"
+    }
+  ];
+  res.send(data);
+};
+
+module.exports.addRecipe = function addRecipe(req, res, next) {
+  res.send({
+    message: 'This is the mockup controller for addRecipe'
+  });
+};
