02a4a5692b7a443c9f7662623a38f55a7f208cd6
PabloFM <pafmon@gmail.com>
2021-12-09 18:47:13 +0100

    error

--- controllers/apiv1recipesControllerService.js
+++ controllers/apiv1recipesControllerService.js
@@ -3,7 +3,7 @@
 module.exports.getRecipes = function getRecipes(req, res, next) {
   var data = [
     {
-      "recipeId": "espetos",
+      "recipeIdS": "espetos",
       "description": "Sardinas a la brasa ensartadas en un palo",
       "ingredients": "Sardina, sal y brasas",
       "complexity": "media"
